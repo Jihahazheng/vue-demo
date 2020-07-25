a31d774882844d7f6eb3f95c5864199994345e14
zhengzhendu <zdzhengzhendu@163.com>
2020-07-25 23:45:25 +0800

    登录页面样式调整

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="login_container">
-        <div class="login_box"></div>
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -25,5 +29,24 @@ export default {
     left: 50%;
     top: 50%;
     transform: translate(-50%,-50%);//向x轴，y轴移动"自身长宽"的50%,从而达到居中的效果
+
+    .avatar_box{
+        height: 130px;
+        width: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background-color: #fff;
+        img {
+            height: 100%;
+            width: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
 }
 </style>
