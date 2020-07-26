0eea521d675e1d8975f8396b1466389f817020b8
zhengzhendu <zdzhengzhendu@163.com>
2020-07-26 16:04:51 +0800

    添加axios请求拦截器，添加token

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 //配置请求根路径
 axios.defaults.baseURL="http://127.0.0.1:8888/api/private/v1/"
+//添加axios请求拦截器，添加token
+axios.interceptors.request.use(config=>{
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  console.log(config);
+  return config;
+})
 // 挂载原型属性
 Vue.prototype.$http= axios
 
