acd2fad9e1039a0c65dc8d0df27de76bad18913c
zhengzhendu <zdzhengzhendu@163.com>
2020-08-09 14:28:12 +0800

    添加富文本编辑器

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -76,7 +76,10 @@
                             <el-button size="small" type="primary">点击上传</el-button>
                         </el-upload>
                     </el-tab-pane>
-                    <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+                    <el-tab-pane label="商品内容" name="4">
+                        <quill-editor v-model="addForm.goods_introduce"></quill-editor>
+                        <el-button type="primary" class="btnAdd">添加商品</el-button>
+                    </el-tab-pane>
                 </el-tabs>
             </el-form>
         </el-card>
@@ -103,6 +106,8 @@ export default {
                 goods_number:0,
                 goods_cat:[],//商品分类
                 pics:[],//图片路径数组
+                //商品详情描述
+                goods_introduce:"",
             },
             addFormRules:{
                 goods_name:[
@@ -240,6 +245,10 @@ export default {
 }
 .previewImg{
     width: 100%;
+}
+
+.btnAdd{
+    margin-top: 10px;
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import './assets/fonts/iconfont.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 import axios from 'axios'
 //配置请求根路径
 axios.defaults.baseURL="http://127.0.0.1:8888/api/private/v1/"
@@ -38,6 +44,9 @@ Vue.filter("dateFomat",function(originVal){
 
   return `${y}-${m}-${d} ${hh}-${mm}-${ss}`;
 })
+
+//将文本编辑器注册成全局可用组件
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
